915c9ad83d3844fcbcdb3b32363c21e63171a241
Charles Hanley <chanley@mathematica-mpr.com>
2019-02-21 16:16:51 -0500

    compute Fibonacci numbers with generating function

--- 06/Fibonacci.hs
+++ 06/Fibonacci.hs
@@ -1,4 +1,5 @@
 {-# OPTIONS_GHC -Wall             #-}
+{-# LANGUAGE FlexibleInstances    #-}
 
 module Fibonacci where
 
@@ -29,22 +30,22 @@ instance Show a => Show (Stream a) where
     show = (++ "...") . show . take 10 . streamToList
 
 streamToList :: Stream a -> [a]
-streamToList (x :~ stream) = x : streamToList stream
+streamToList (a :~ stream) = a : streamToList stream
 
 ----------
 -- 4
 
 streamRepeat :: a -> Stream a
-streamRepeat x = x :~ streamRepeat x
+streamRepeat a = a :~ streamRepeat a
 
 streamMap :: (a -> b) -> Stream a -> Stream b
-streamMap f (x :~ xs) = f x :~ streamMap f xs
+streamMap f (a :~ as) = f a :~ streamMap f as
 
 instance Functor Stream where
     fmap = streamMap
 
 streamFromSeed :: (a -> a) -> a -> Stream a
-streamFromSeed f x = x :~ streamFromSeed f (f x)
+streamFromSeed f a = a :~ streamFromSeed f (f a)
 
 ----------
 -- 5
@@ -58,7 +59,7 @@ ruler = go 0
           go n = interleaveStreams (streamRepeat n) (go (n + 1))
 
 interleaveStreams :: Stream a -> Stream a -> Stream a
-interleaveStreams (x :~ xs) ys = x :~ interleaveStreams ys xs
+interleaveStreams (a :~ as) bs = a :~ interleaveStreams bs as
 
 -- cannot be the below because it pattern matches on both args, and when using
 -- it in ruler, evaluating second arg never terminates
@@ -70,6 +71,25 @@ interleaveStreams (x :~ xs) ys = x :~ interleaveStreams ys xs
 -- 6
 
 x :: Stream Integer
+x = 0 :~ 1 :~ streamRepeat 0
+
+instance Num (Stream Integer) where
+    fromInteger i    = i :~ streamRepeat 0
+    negate (i :~ is) = negate i :~ negate is
+    abs              = undefined
+    signum           = undefined
+    (a :~ as) +     (b :~ bs) = (a + b) :~ (as + bs)
+    (a :~ as) * bs0@(b :~ bs) = (a * b) :~ (streamMap (* a) bs + (as * bs0))
+
+instance Fractional (Stream Integer) where
+    (a :~ as) / (b :~ bs) = q
+        where q = (a `div` b) :~ streamMap (`div` b) (as - (q * bs))
+    fromRational = undefined
+
+fibs3 :: Stream Integer
+fibs3 = x / (1 - x - x^(2 :: Integer))
+
+-- Wow, dark mathemagics.
 
 ----------
 -- 7
